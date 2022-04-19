efc25186c59a497a99bcd92bcb976a49226393aa
smnbzs <simibalazs0327@gmail.com>
2022-04-19 17:41:30 +0200

    jsfeladtamxd

--- weboldal/main.js
+++
@@ -1,7 +0,0 @@
-let kosarikon = document.querySelector("#bevasarlokocsi");
-let kosar = document.querySelector(".kosar");
-let bezar = document.querySelector("#bezar");
-
-kosarikon.onclick = () => {
-  kosar.classList.add("active");
-};
