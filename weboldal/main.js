c5b4c19b84abea9d8c8d91e1a0fbe7c748904ca8
smnbzs <simibalazs0327@gmail.com>
2022-04-19 16:38:48 +0200

    egykisrésznemműködikdehaladokvele

---
+++ weboldal/main.js
@@ -0,0 +1,7 @@
+let kosarikon = document.querySelector("#bevasarlokocsi");
+let kosar = document.querySelector(".kosar");
+let bezar = document.querySelector("#bezar");
+
+kosarikon.onclick = () => {
+  kosar.classList.add("active");
+};
